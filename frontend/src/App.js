133ab466b7cc0d09666de960a5c8f1eab5fc72ac
THÔNG MINH <huynhminhthong1912@gmail.com>
2021-07-21 12:53:17 +0700

    share code on github

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
+
+function App() {
+  return (
+    <BrowserRouter>
+        <div className="grid-container">
+          <header className="row">
+            <div>
+              <a className="brand" href="/">ThongShop</a>
+            </div>
+            <div>
+              <a href="/cart">Cart</a>
+              <a href="/signin">Sign In</a>
+            </div>
+          </header>
+          <main>
+            <Route exact path="/product/:id" component={ProductScreen}></Route>
+            <Route exact path="/" component={HomeScreen}></Route>
+          </main>
+          <footer className="row center">
+            All right reserved.
+          </footer>
+        </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Product from '../components/Product'
+import data from '../data';
+
+export default function HomeScreen() {
+    return (
+        <div>
+            <div className="row center">
+              {data.products.map((product, index) => {
+                return <Product key={index} product={product} />
+              })}
+            </div>
+        </div>
+    )
+}
